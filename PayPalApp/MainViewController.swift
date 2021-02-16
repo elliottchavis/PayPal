722439657519f6b5451db3153df7912418c61a3b
agileassociates <agileassociates@agileastessMini.lan>
2021-02-15 22:46:48 -0500

    added top nav buttons & shadows

--- PayPalApp/MainViewController.swift
+++ PayPalApp/MainViewController.swift
@@ -18,9 +18,28 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var balanceView: UIView!
+    @IBOutlet weak var getMoreView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getMoreView.layer.cornerRadius = CGFloat(10.0)
+        balanceView.layer.cornerRadius = CGFloat(5.0)
+        balanceView.layer.shadowOpacity = Float(0.2)
+        balanceView.layer.shadowOffset = CGSize(width: 2, height: 1)
+        balanceView.layer.shadowRadius = CGFloat(2)
+        
+        var rightBarItemImage = UIImage(named: "settings_icon")
+        rightBarItemImage = rightBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarItemImage, style: .plain, target: nil, action: nil)
+        
+        let profileImageView = UIImageView(image: UIImage(named: "profile_image"))
+        self.navigationItem.titleView = profileImageView
+        
+        var leftBarItemImage = UIImage(named: "notifications_icon")
+        leftBarItemImage = leftBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftBarItemImage, style: .plain, target: nil, action: nil)
         
         
     }
